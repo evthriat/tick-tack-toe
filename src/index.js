72e72db961595e84344520321d9691d84ea4c3c0
Evan <evantatkinson@gmail.com>
2017-12-30 23:05:57 -0800

    added css details, fixed box size problem

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import'./index.css';
 
 function Square(props){
     return (
-      <button className="square" onClick={() => 
-      	this.props.onClick()}>
-        {this.props.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
 }
@@ -16,25 +15,30 @@ class Board extends React.Component {
 		super(props);
 		this.state = {
 			squares: Array(9).fill(null),
+			xIsNext: true,
 		};
 	}
- 
- 	handleClick(i) {
- 		const squares = this.state.squares.slice();
- 		squares[i] = 'X';
- 		this.setState({squares: squares});
- 	}
-  renderSquare(i) {
-    return (
-    	<Square 
-    		value={this.state.squares[i]} 
-    		onClick={()=> this.handleClick(i)}
-    	/>
-    );
-  }
+	 
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		squares[i] = 'X';
+		this.setState({
+			squares: squares,
+			xIsNext: !this.state.xIsNext,
+		});
+
+	}
+	renderSquare(i) {
+	return (
+		<Square 
+			value={this.state.squares[i]} 
+			onClick={()=> this.handleClick(i)}
+		/>
+	);
+	}
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
