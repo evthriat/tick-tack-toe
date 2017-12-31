25f664aca7cb0b997bea6bc88a4dd2bc473492c1
Evan <evantatkinson@gmail.com>
2017-12-30 22:31:16 -0800

    made clicking board pop alert

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import'./index.css';
 class Square extends React.Component {
   render(props) {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('this is clicked')}>
         {this.props.value}
       </button>
     );
